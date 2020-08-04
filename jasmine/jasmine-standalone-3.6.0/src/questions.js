8564c19387ec8b147bc51fa918b21d0b9e2880c4
anna dodo <anna.dorenskaya@gmail.com>
2020-08-04 22:45:34 +0200

    added the base of the second section

---
+++ jasmine/jasmine-standalone-3.6.0/src/questions.js
@@ -0,0 +1,80 @@
+const questionsList = [
+    {
+        question: "Who was the messenger of the gods?",
+        answers: [
+            { answer: 'Hermes', correct: true},
+            { answer: 'Telegram', correct: false},
+            { answer: 'Whatsup', correct: false},
+            { answer: 'FB messanger', correct: false}
+        ]
+    }, 
+    {
+        question: 'What sport is featured in the video game “FIFA”?',
+        answers: [
+            { answer: 'Tennis', correct: false},
+            { answer: 'Volleyball', correct: false},
+            { answer: 'Football', correct: true},
+            { answer: 'Carting', correct: false}
+        ]
+    }, 
+    {
+        question: "What does Au stand for in the periodic table?",
+        answers: [
+            { answer: 'Still', correct: false},
+            { answer: 'Silver', correct: false},
+            { answer: 'Gold', correct: true},
+            { answer: 'Aluminium', correct: false}
+        ]
+    }, 
+    {
+        question: 'What is the capital city of Iceland?',
+        answers: [
+            { answer: 'Berlin', correct: false},
+            { answer: 'Saint-Petersburg', correct: false},
+            { answer: 'Amsterdam', correct: false},
+            { answer: 'Rekyavik', correct: true}
+        ]
+    }
+] 
+ 
+/* class Questions = {
+    constructor(question, answer, )
+} */
+/*  const questions = {
+    {
+        question: "Who was the messenger of the gods?",
+        answers: [
+            { answer: 'Hermes', correct: true},
+            { answer: 'Telegram', correct: false},
+            { answer: 'Whatsup', correct: false},
+            { answer: 'FB messanger', correct: false}
+        ]
+    }, 
+    {
+        question: 'What sport is featured in the video game “FIFA”?',
+        answers: [
+            { answer: 'Tennis', correct: false},
+            { answer: 'Volleyball', correct: false},
+            { answer: 'Football', correct: true},
+            { answer: 'Carting', correct: false}
+        ]
+    }, 
+    {
+        question: "What does Au stand for in the periodic table?",
+        answers: [
+            { answer: 'Still', correct: false},
+            { answer: 'Silver', correct: false},
+            { answer: 'Gold', correct: true},
+            { answer: 'Aluminium', correct: false}
+        ]
+    }, 
+    {
+        question: 'What is the capital city of Iceland?',
+        answers: [
+            { answer: 'Berlin', correct: false},
+            { answer: 'Saint-Petersburg', correct: false},
+            { answer: 'Amsterdam', correct: false},
+            { answer: 'Rekyavik', correct: true}
+        ]
+    }
+ } */
--- jasmine/jasmine-standalone-3.6.0/src/script.js
+++ jasmine/jasmine-standalone-3.6.0/src/script.js
@@ -4,6 +4,7 @@
 let greetingButton = document.getElementsByClassName('name-btn')[0]
 let inputName = document.getElementById('stranger-name');
 let parentGreetingElement = document.getElementsByClassName('hello-section')[0]
+let startBtn = document.createElement('button')
 
 //check that a user put the name
 function strangerNameLength() {
@@ -14,13 +15,20 @@ function addPAndStartButton() {
   let p = document.createElement('p')
   p.appendChild(document.createTextNode(`${inputName.value}! I have a nice game for you! If you wanna play - click the Start button!`))
   parentGreetingElement.appendChild(p)
-  let startBtn = document.createElement('button')
+  //let startBtn = document.createElement('button')
   parentGreetingElement.appendChild(startBtn)
+  startBtn.classList.add('start-btn')
+  startBtn.innerHTML = 'LET\'S GO!!'
+  //debug
+  console.log(startBtn)
+  return startBtn
+
 }
 //if a user submitted the input by Enter
 function greetAfterKeypress(event) {
   if (strangerNameLength() > 0 && event.keyCode === 13) {
     addPAndStartButton();
+   // let letgoButton = document.getElementsByClassName('start-btn')[0]
   } else if (event.keyCode === 13) {
     alert('please, put your name');
   }
@@ -29,6 +37,8 @@ function greetAfterKeypress(event) {
 function greetAfterClick() {
   if (strangerNameLength() > 0) {
     addPAndStartButton();
+   // let letgoButton = document.getElementsByClassName('start-btn')[0]
+    
   } else {
     alert('please, put your name');
   }
@@ -36,4 +46,35 @@ function greetAfterClick() {
 //add listeners to greet a user
 inputName.addEventListener('keypress', greetAfterKeypress)
 greetingButton.addEventListener('click', greetAfterClick)
+
+//add listener for the start button
+startBtn.addEventListener('click', function() {
+  addQuizSection();
+}) 
+
+
+
+//working with the new elements in the quiz page
+let bannerSectionQuiz = document.createElement('div')
+let bannerParent = document.getElementsByClassName('parent-section')[0]
+
+//after click of the start button -  generate the 1st section of the game
+function addQuizSection() {
+  let bodyParent = document.querySelector('body')
+  //create a background image
+/*   bannerSectionQuiz.classList.add('banner-quiz')
+  bannerParent.appendChild(bannerSectionQuiz); */
+  quizHolers();
+}
+
+//generate the questions and answers holders
+let questionHolder = document.querySelector('#question-container');
+let quizSection = document.getElementsByClassName('quiz')[0]
+
+function quizHolers() {
+  quizSection.classList.toggle('hide')
+  
+}
+
+
 
